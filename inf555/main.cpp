d105c565944d240ec92314e898414d7a44746e74
Basile Bruneau <basilebruneau@gmail.com>
2015-11-04 16:43:07 +0100

    Initial Commit

---
+++ inf555/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inf555
+//
+//  Created by Basile Bruneau on 04/11/2015.
+//  Copyright © 2015 Basile Camille. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
